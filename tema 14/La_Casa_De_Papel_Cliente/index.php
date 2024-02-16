e23254e992a8c16281a55d92f18b879ab743255e
Adrian Jacek <adrianjacek@iesluisbraille.es>
2024-02-16 10:10:33 +0100

    funcion API

--- tema 14/La_Casa_De_Papel_Cliente/index.php
+++ tema 14/La_Casa_De_Papel_Cliente/index.php
@@ -1,46 +1,20 @@
 <?php
-
     include("template/cabecera.php");
-
 
    if(isset($_POST['filtrado-id']) && trim($_POST['filtrado-id'])!=''){
 
-
             if(str_contains($_POST['filtrado-id']," ")){
-                   // echo "bien";
-                $_POST['filtrado-id']=str_replace(" ","%20", $_POST['filtrado-id']);
-       }
 
-                //echo  $_POST['filtrado-id'];
+                $_POST['filtrado-id']=str_replace(" ","%20", $_POST['filtrado-id']);
+            }
+
        $url="http://localhost/2DAW/PHP/tema%2014/La_Casa_De_Papel/index.php/Personajes/".$_POST['filtrado-id'];
 
-
-     //  $_POST['filtrado-id']
    }else{
        $url="http://localhost/2DAW/PHP/tema%2014/La_Casa_De_Papel/index.php/Personajes";
    }
 
-  // $url=urlencode($url);
-
-    $objCurl = curl_init();
-    curl_setopt($objCurl, CURLOPT_RETURNTRANSFER, 1);
-
-
-           curl_setopt($objCurl, CURLOPT_URL, $url);
-
-           // Configurar opciones para devolver el resultado como una cadena
-
-           // Ejecutar la solicitud y obtener la respuesta
-           $objJSON = curl_exec($objCurl);
-
-           // Comprobar errores
-           if (curl_errno($objCurl)) {
-               echo 'Error en la solicitud cURL: ' . curl_error($objCurl);
-
-           } else {
-
-               $arrPersonajes = json_decode($objJSON,true);
-
+        $arrPersonajes=recogerAPI($url);
                echo "<table >";
 
                //bucle que imprime las cabeceras
@@ -60,10 +34,23 @@
                    echo "<tr>";
                }
                echo "</table>";
-           }
 
-      curl_close($objCurl);
+            function recogerAPI($url){
+                $objCurl = curl_init();
+                curl_setopt($objCurl, CURLOPT_RETURNTRANSFER, 1);
 
+                curl_setopt($objCurl, CURLOPT_URL, $url);
 
+                $objJSON = curl_exec($objCurl);
 
+                if (curl_errno($objCurl)) {
+                    echo 'Error en la solicitud cURL: ' . curl_error($objCurl);
 
+                }else{
+
+                    $arrPersonajes = json_decode($objJSON,true);
+
+                    curl_close($objCurl);
+                    return $arrPersonajes;
+                }
+            }
